60fbcc5bf309212fa6318dac17c80be50c8fa74b
Sal Niro <salvatore.niro@outlook.com>
2017-08-22 12:12:17 -0400

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+app.set('port', 3000);
+app.use('/', express.static('.'));
+
+app.all('/*', function (req, res, next) {
+  res.sendFile('index.html', {root: __dirname});
+});
+
+app.listen(app.get('port'), function () {
+  console.log(`craigfus.co local express server running on port ${app.get('port')}`);
+});
